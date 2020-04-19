8d10f93b1e841c7cd8aca87f195ddf4c125bc4c4
echeniquegrecia <echeniquegrecia@gmail.com>
2020-04-20 00:30:05 +0200

    feat: add search client

--- graphic_interface/menus/client/table_search_client.py
+++ graphic_interface/menus/client/table_search_client.py
@@ -22,34 +22,40 @@ class TableSearchClient(BaseFrame):
         }
 
         frame_1 = tk.LabelFrame(self.root, text="Buscar Client", width=100, height=10)
-        frame_1.pack(side='left', ipadx=200, padx=5, pady=5, fill='y')
+        frame_1.pack(side='top', padx=5, pady=5, fill='both', expand=True)
 
-        frame_2 = tk.LabelFrame(self.root)
-        frame_2.pack(side='right', padx=5, pady=11, fill='both')
+        frame_2 = tk.LabelFrame(frame_1)
+        frame_2.pack(side='top', padx=5, pady=11, fill='both')
+
+        frame_3 = tk.LabelFrame(frame_1)
+        frame_3.pack(side='top', padx=5, pady=11, fill='both', expand=True)
 
         self.option_var = tk.StringVar()
         self.option_var.set("--Seleccione una busqueda--")
         contents = {"Nombre", "Apellido", "Cédula", "Email"}
-        self.options = tk.OptionMenu(frame_1, self.option_var, *contents)
+        self.options = tk.OptionMenu(frame_2, self.option_var, *contents)
         self.options.config(font=('Helvetica', 15))
-        self.options.pack(side='top', pady=10, padx=10, fill='x')
+        self.options.pack(side='left', pady=10, padx=10, fill='x')
 
         self.entry_var = tk.StringVar()
-        entry = tk.Entry(frame_1, font="Helvetica 15", textvariable=self.entry_var)
-        entry.pack(side='top', pady=10, padx=10, fill='x')
+        entry = tk.Entry(frame_2, font="Helvetica 15", textvariable=self.entry_var)
+        entry.pack(side='left', pady=10, padx=10, fill='x')
 
-        button_3 = tk.Button(frame_1, text="Buscar", font='Helvetica 15 bold', command=self.update_table)
-        button_3.pack(side='top', pady=10, padx=10, fill='x')
+        button_3 = tk.Button(frame_2, text="Buscar", font='Helvetica 15 bold', command=self.search_client_by_category)
+        button_3.pack(side='left', pady=10, padx=10, fill='x')
 
-        frame_3 = tk.Frame(frame_1)
-        frame_3.pack(side='bottom', fill='x')
+        button_4 = tk.Button(frame_2, text="Limpiar", font='Helvetica 15 bold', command=self.clean_table)
+        button_4.pack(side='left', pady=10, padx=10, fill='x')
 
-        button_4 = tk.Button(frame_3, text="Regresar", font='Helvetica 15 bold', command=self.go_back)
-        button_4.pack(side='left', pady=10, padx=10)
+        frame_4 = tk.Frame(frame_1)
+        frame_4.pack(side='bottom', fill='x')
+
+        button_5 = tk.Button(frame_4, text="Regresar", font='Helvetica 15 bold', command=self.go_back)
+        button_5.pack(side='right', pady=10, padx=10)
 
         # Define Heading table
         columns = ("Id", "Nombre", "Apellido", "Cédula", "Email", "Teléfono fijo", "Celular", "Direccion")
-        self.treeview = ttk.Treeview(frame_2, height=18, show="headings", columns=columns)
+        self.treeview = ttk.Treeview(frame_3, height=18, show="headings", columns=columns)
         self.treeview.heading("Id", text="Id")
         self.treeview.heading("Nombre", text="Nombre")
         self.treeview.heading("Apellido", text="Apellido")
@@ -92,12 +98,12 @@ class TableSearchClient(BaseFrame):
                 address[client]))
 
         # Scrollbar Vertical
-        scrollbar_vertical = ttk.Scrollbar(frame_2)
+        scrollbar_vertical = ttk.Scrollbar(frame_3)
         scrollbar_vertical.configure(command=self.treeview.yview)
         self.treeview.configure(yscrollcommand=scrollbar_vertical.set)
 
         # Scrollbar Horizontal
-        scrollbar_horizontal = ttk.Scrollbar(frame_2, orient="horizontal")
+        scrollbar_horizontal = ttk.Scrollbar(frame_3, orient="horizontal")
         scrollbar_horizontal.configure(command=self.treeview.xview)
         self.treeview.configure(xscrollcommand=scrollbar_horizontal.set)
 
@@ -108,24 +114,19 @@ class TableSearchClient(BaseFrame):
 
         self.root.mainloop()
 
-    def get_item_to_search(self):
-        """Searh client."""
-        test = {
+    def get_client_by_item(self):
+        """Get client."""
+        item = {
             "Nombre": self.client.get_by_name(name=self.entry_var.get()),
-            "Apellido": "get_by_last_name",
-            "Cédula": "get_by_identity_card",
-            "Email": "get_by_email"
+            "Apellido": self.client.get_by_last_name(last_name=self.entry_var.get()),
+            "Cédula": self.client.get_by_identity_card(identity_card=self.entry_var.get()),
+            "Email": self.client.get_by_email(email=self.entry_var.get())
         }
-        return test[self.option_var.get()]
+        return item[self.option_var.get()]
 
-    def go_back(self):
-        """Go back to Menu Client."""
-        self.hide()
-        self.master.show()
-
-    def update_table(self):
-        """Update table."""
-        clients = self.get_item_to_search()
+    def search_client_by_category(self):
+        """Search client by category."""
+        clients = self.get_client_by_item()
         id = [client.get("client_id") for client in clients]
         name = [client.get("name") for client in clients]
         last_name = [client.get("last_name") for client in clients]
@@ -148,3 +149,34 @@ class TableSearchClient(BaseFrame):
                 phone_1[client],
                 phone_2[client],
                 address[client]))
+
+    def clean_table(self):
+        """Clean table."""
+        clients = self.client.get_all()
+        id = [client.get("client_id") for client in clients]
+        name = [client.get("name") for client in clients]
+        last_name = [client.get("last_name") for client in clients]
+        identity_card = [client.get("identity_card") for client in clients]
+        email = [client.get("email") for client in clients]
+        phone_1 = [client.get("phone_1") for client in clients]
+        phone_2 = [client.get("phone_2") for client in clients]
+        address = [client.get("address") for client in clients]
+
+        for i in self.treeview.get_children():
+            self.treeview.delete(i)
+
+        for client in range(0, len(clients)):
+            self.treeview.insert('', client, values=(
+                id[client],
+                name[client],
+                last_name[client],
+                identity_card[client],
+                email[client],
+                phone_1[client],
+                phone_2[client],
+                address[client]))
+
+    def go_back(self):
+        """Go back to Menu Client."""
+        self.hide()
+        self.master.show()
