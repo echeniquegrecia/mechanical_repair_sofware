340c57f9caddb1ef3a7cc202880ab572d6a785d8
Grecia <echeniquegrecia@gmail.com>
2021-12-04 01:16:48 +0100

    fix: add singlenton for database

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import sqlite3
 
 from backend.database.schema import CLIENTS
@@ -7,34 +8,49 @@ from backend.database.schema import VEHICLES
 from backend.database.schema import REPAIRS
 
 
-class Database:
+class Singleton(type):
+    """Singleton class for database."""
+    _instances = {}
 
-    def __init__(self, database: str):
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class DatabaseConnection(metaclass=Singleton):
+    """DatabaseConnection class."""
+
+    def __init__(self, database_path):
         """Database init."""
-        self.database = database
-        self.connection = None
+        self.database_path = database_path
+        self.connection = self.get_connection()
 
     def get_connection(self):
         """Get Database connection."""
         try:
-            # import pdb; pdb.set_trace()
-            if not os.path.isfile(self.database):
-                self.connection = sqlite3.connect(self.database)
-                self.create_table(CLIENTS)
-                self.create_table(VEHICLES_TYPE)
-                self.create_table(VEHICLES)
-                self.create_table(REPAIRS)
-            self.connection = sqlite3.connect(self.database)
-            return self.connection
+            if not os.path.isfile(self.database_path):
+                # Create db and tables in case they do not exist.
+                connection = sqlite3.connect(self.database_path)
+                self.create_table(connection, CLIENTS)
+                self.create_table(connection, VEHICLES_TYPE)
+                self.create_table(connection, VEHICLES)
+                self.create_table(connection, REPAIRS)
+            connection = sqlite3.connect(self.database_path)
+            logging.info("Database initialized.")
         except ConnectionError as error:
-            return error
+            logging.error(f"Database error: {error}.")
+            raise error
         finally:
-            sqlite3.connect(self.database).close()
+            connection.close()
+        return connection
 
-
-    def create_table(self, table):
-        """Create a table."""
+    def create_table(self, connection, table):
+        """Create a table in Database."""
         try:
-            self.connection.cursor().execute(table)
+            connection.cursor().execute(table)
         except ConnectionError as error:
-            return error
+            logging.error(f"Database creation table error: {error}.")
+            raise error
+
